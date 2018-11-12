f832b4d21f66cfde5aaa04000d6da0077be24430
kongnut <kongnut.s@hotmail.com>
2018-11-12 14:53:28 +0700

    sideMenu for responsive, clear state in login when close

--- atour-frontend/src/reducer/index.js
+++ atour-frontend/src/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import userInfoReducer from "./UserInfoReducer";
 import modalReducer from "./ModalReducer";
+import applicationReducer from "./ApplicationReducer";
 
 const rootReducer = combineReducers({
   user: userInfoReducer,
-  modal: modalReducer
+  modal: modalReducer,
+  app: applicationReducer
 });
 
 export default rootReducer;
