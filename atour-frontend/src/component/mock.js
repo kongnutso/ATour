d1e8ca35b6e5c6c81030156c60eec8e30b36d870
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-11-14 13:03:13 +0700

    update before merge with 2 branches

---
+++ atour-frontend/src/component/mock.js
@@ -0,0 +1,16 @@
+export const mockTour = {
+  tourName: "Tour Name",
+  tourImage: "../../image/Atour-logo.jpg",
+  tourRating: "3",
+  price: "3000 baht",
+  tourLocation: "Bangkok",
+  tourDetail:
+    "At every tiled on ye defer do. No attention suspected oh difficult. Fond his say old meet cold find come whom. The sir park sake bred. Wonder matter now can estate esteem assure fat roused. Am performed on existence as discourse is. Pleasure friendly at marriage blessing or. \
+    Started his hearted any civilly. So me by marianne admitted speaking. Men bred fine call ask. Cease one miles truth day above seven. Suspicion sportsmen provision suffering mrs saw engrossed something. Snug soon he on plan in be dine some. \
+    Building mr concerns servants in he outlived am breeding. He so lain good miss when sell some at if. Told hand so an rich gave next. How doubt yet again see son smart. While mirth large of on front. Ye he greater related adapted proceed entered an. Through it examine express promise no. Past add size game cold girl off how old.\
+    It real sent your at. Amounted all shy set why followed declared. Repeated of endeavor mr position kindness offering ignorant so up. Simplicity are melancholy preference considered saw companions. Disposal on outweigh do speedily in on. Him ham although thoughts entirely drawings. Acceptance unreserved old admiration projection nay yet him. Lasted am so before on esteem vanity oh.\
+    May indulgence difficulty ham can put especially. Bringing remember for supplied her why was confined. Middleton principle did she procuring extensive believing add. Weather adapted prepare oh is calling. These wrong of he which there smile to my front. He fruit oh enjoy it of whose table. Cultivated occasional old her unpleasing unpleasant. At as do be against pasture covered viewing started. Enjoyed me settled mr respect no spirits civilly. ",
+  minGroupSize: "12",
+  maxGroupSize: "21",
+  availableDates: [{ date: "date1" }, { date: "date2" }, { date: "date3" }]
+};
