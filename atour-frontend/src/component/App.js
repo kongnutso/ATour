feeab6c63993c9a876680f9cb5ca5df3470c2be5
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-11-14 13:30:49 +0700

    merged all with GuideHome, GuideViewPublishedTour, and GuideTourInfo

--- atour-frontend/src/component/App.js
+++ atour-frontend/src/component/App.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import { mockTour } from "./mock";
 import GuideTourInfo from "./Guide/GuideTourInfo/GuideTourInfo";
 import GuideViewPublishedTour from "../component/Guide/GuideViewPublishedTour";
+import GuideHome from "../component/Guide/GuideHome";
 
 class App extends Component {
   render() {
     return (
       <div className="App" tour>
-        {/* <GuideTourInfo tour={mockTour} /> */}
-        <GuideViewPublishedTour />
+        <GuideTourInfo tour={mockTour} />
+        {/* <GuideViewPublishedTour /> */}
+        {/* <GuideHome /> */}
       </div>
     );
   }
