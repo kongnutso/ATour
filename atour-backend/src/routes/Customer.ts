7df22102dcc1bbf88fdad1f7021fded61a03d10b
Wattanai <wattanai.tha@gmail.com>
2018-11-14 23:50:08 +0700

    get guide profile & searchTour

--- atour-backend/src/routes/Customer.ts
+++ atour-backend/src/routes/Customer.ts
@@ -1,161 +1,154 @@
 import * as express from 'express';
 import { Db } from 'mongodb';
-import { 
-    saveCustomer,
-    checkCustomerUsernameDuplicate,
-    editCustomerProfile,
-    login,
-    saveCustomerToken,
-    getCustomerToken,
-    getCustomerProfile
+import {
+  saveCustomer,
+  checkCustomerUsernameDuplicate,
+  editCustomerProfile,
+  login,
+  saveCustomerToken,
+  getCustomerToken,
+  getCustomerProfile
 } from '../repository/Customer';
 import {
-    registerCustomerService,
-    loginService,
-    editCustomerProfileService,
-    getCustomerProfileService,
+  registerCustomerService,
+  loginService,
+  editCustomerProfileService,
+  getCustomerProfileService
 } from '../service/CustomerService';
-import { searchTourService, searchGuideService } from '../service/CustmerSearchService';
+import {
+  searchTourService,
+  searchGuideService
+} from '../service/CustmerSearchService';
 import { searchTour, searchGuide } from '../repository/CustomerSearch';
 
 import * as uuid from 'uuid/v4';
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('Hello Customer');
+router.get('/', (req, res) => {
+  res.send('Hello Customer');
 });
 
-router.post('/register', async (req,res) => {
-    try {
-        const db: Db = res.locals.db;
-        const {
-            userName,
-            password,
-            email,
-            firstName,
-            lastName,
-            personalId,
-            phoneNumber,
-            birthDate,
-            gender
-        } = req.body;
-        const customer = await registerCustomerService(
-            checkCustomerUsernameDuplicate(userName, db),
-            saveCustomer(db),
-            saveCustomerToken(db),
-            () => uuid()
-        )(
-                userName,
-                password,
-                email,
-                firstName,
-                lastName,
-                personalId,
-                phoneNumber,
-                birthDate,
-                gender
-            );
-        res.json(customer);
-    }catch(e){
-        console.log(e.message)
-        res.json({customer:null, error: e.message})
-    }
+router.post('/register', async (req, res) => {
+  try {
+    const db: Db = res.locals.db;
+    const {
+      userName,
+      password,
+      email,
+      firstName,
+      lastName,
+      personalId,
+      phoneNumber,
+      birthDate,
+      gender
+    } = req.body;
+    const customer = await registerCustomerService(
+      checkCustomerUsernameDuplicate(userName, db),
+      saveCustomer(db),
+      saveCustomerToken(db),
+      () => uuid()
+    )(
+      userName,
+      password,
+      email,
+      firstName,
+      lastName,
+      personalId,
+      phoneNumber,
+      birthDate,
+      gender
+    );
+    res.json(customer);
+  } catch (e) {
+    console.log(e.message);
+    res.json({ customer: null, error: e.message });
+  }
 });
 
-router.post('/login', async (req,res) => {
-    try {
-        const db: Db = res.locals.db;
-        const {
-            userName,
-            password
-        } = req.body;
-        const token = await loginService(
-            login(db), getCustomerToken(db))(
-                userName,
-                password
-            );
-        res.json(token);
-    } catch (error) {
-        console.log(error.message)
-        res.json({token:null, error: error.message})
-    }
-})
-
-router.post('/editProfile', async (req,res) => {
-    try {
-        const db: Db = res.locals.db;
-        const {
-            userName,
-            firstName,
-            lastName,
-            phoneNumber,
-            birthDate,
-            gender
-        } = req.body;
-        const profile = await editCustomerProfileService(
-            editCustomerProfile(db))(
-                userName,
-                firstName,
-                lastName,
-                phoneNumber,
-                birthDate,
-                gender
-            );
-        res.json(profile);
-    } catch (error) {
-        console.log(error.message)
-        res.json({profile:null, error:error.message})
-    }
-        
+router.post('/login', async (req, res) => {
+  try {
+    const db: Db = res.locals.db;
+    const { userName, password } = req.body;
+    const token = await loginService(login(db), getCustomerToken(db))(
+      userName,
+      password
+    );
+    res.json(token);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ token: null, error: error.message });
+  }
 });
 
-router.post('/getProfile', async (req,res) => {
-    try {
-        const db: Db = res.locals.db;
-        const {
-            userName
-        } = req.body;
-        const profile = await getCustomerProfileService(
-            getCustomerProfile(db))(
-                userName
-            );
-        res.json(profile);
-    } catch (error) {
-        console.log(error.message)
-        res.json({profile:null, error:error.message})
-    }
-        
+router.post('/editProfile', async (req, res) => {
+  try {
+    const db: Db = res.locals.db;
+    const {
+      userName,
+      firstName,
+      lastName,
+      phoneNumber,
+      birthDate,
+      gender
+    } = req.body;
+    const profile = await editCustomerProfileService(editCustomerProfile(db))(
+      userName,
+      firstName,
+      lastName,
+      phoneNumber,
+      birthDate,
+      gender
+    );
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ profile: null, error: error.message });
+  }
 });
 
-router.post('/searchTour', async (req,res) => {
-    try {
-        const db:Db = res.locals.db;
-        const {keyword} = req.body;
-        const results = await searchTourService(searchTour(db))(
-            keyword
-        );
-        res.json(results);
-    } catch (error) {
-        console.log(error.message)
-        res.json({results:null, error: error.message})
-    }
+router.post('/getProfile', async (req, res) => {
+  try {
+    const db: Db = res.locals.db;
+    const { userName } = req.body;
+    const profile = await getCustomerProfileService(getCustomerProfile(db))(
+      userName
+    );
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ profile: null, error: error.message });
+  }
+});
 
-})
+router.get('/searchTours', async (req, res) => {
+  const db: Db = res.locals.db;
+  const cursor = await db.collection('tour').find();
+  const tours = await cursor.toArray();
+  res.json(tours);
+});
 
-router.post('/searchGuide', async (req,res) => {
-    try {
-        const db:Db = res.locals.db;
-        const {keyword} = req.body;
-        const results = await searchGuideService(searchGuide(db))(
-            keyword
-        );
-        res.json(results);
-    } catch (error) {
-        console.log(error.message)
-        res.json({results:null, error: error.message})
-    }
-        
-})
+router.post('/searchTour', async (req, res) => {
+  try {
+    const db: Db = res.locals.db;
+    const { keyword } = req.body;
+    const results = await searchTourService(searchTour(db))(keyword);
+    res.json(results);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ results: null, error: error.message });
+  }
+});
 
+router.post('/searchGuide', async (req, res) => {
+  try {
+    const db: Db = res.locals.db;
+    const { keyword } = req.body;
+    const results = await searchGuideService(searchGuide(db))(keyword);
+    res.json(results);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ results: null, error: error.message });
+  }
+});
 
 export default router;
--- atour-backend/src/server.ts
+++ atour-backend/src/server.ts
@@ -1,24 +1,14 @@
 import app from './app';
 import { initMongo } from './db';
-import { Db } from 'mongodb';
 import Tour from './routes/Tour';
 import Guide from './routes/Guide';
 import Customer from './routes/Customer';
 
 const PORT = 3000;
 
-async function testDb(db: Db) {
-  const tourCollection = db.collection('tour');
-  await tourCollection.deleteMany({});
-  await tourCollection.insert({ hello: 'tour' });
-  const tour = await tourCollection.findOne({ hello: 'tour' });
-  console.log(tour);
-}
-
 async function main() {
   const { db } = await initMongo();
 
-  testDb(db);
   app.use((req, res, next) => {
     res.locals.db = db;
     next();
