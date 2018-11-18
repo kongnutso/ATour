fc1a8a27fba303317070df9ba2a06662dff7a2c1
jitimonsun <sun.sjm30@gmail.com>
2018-11-19 02:33:26 +0700

    add search for empty string

--- atour-backend/src/repository/CustomerSearch.ts
+++ atour-backend/src/repository/CustomerSearch.ts
@@ -2,7 +2,7 @@ import {
     Tour ,
     Guide
 } from '../domain/types';
-import { Db } from 'mongodb';
+import { Db, Cursor } from 'mongodb';
 
 export type SearchTourDb = (keyword: string) => Promise<Tour[]>;
 export type SearchGuideDb = (keyword: string) => Promise<Guide[]>;
@@ -10,19 +10,32 @@ export type SearchGuideDb = (keyword: string) => Promise<Guide[]>;
 
 export function searchTour(db:Db):SearchTourDb {
     return async (keyword) =>{
-        const keywords: RegExp[] = keyword.split(' ').map((word) => new RegExp(word));
-        const cursor = await db.collection('tour').find({ tourName: {$in : keywords} });
-        const results = await cursor.toArray();
-        return results;
+        if (keyword === ''){
+            const cursor = await db.collection('tour').find().limit(9);
+            const results = await cursor.toArray();
+            return results;
+        }else {
+            const keywords: RegExp[] = keyword.split(' ').map((word) => new RegExp(word));
+            const cursor = await db.collection('tour').find({ tourName: {$in : keywords} });
+            const results = await cursor.toArray();
+            return results;            
+        }
+        
     }
 }
 
 export function searchGuide(db:Db):SearchGuideDb {
     return async (keyword) =>{
-        const keywords: RegExp[] = keyword.split(' ').map((word) => new RegExp(word));
-        const cursor = await db.collection('guide').find({ userName: {$in : keywords} });
-        const results = await cursor.toArray();
-        return results;
+        if (keyword === ''){
+            const cursor = await db.collection('guide').find().limit(9);
+            const results = await cursor.toArray();
+            return results;
+        }else {
+            const keywords: RegExp[] = keyword.split(' ').map((word) => new RegExp(word));
+            const cursor = await db.collection('guide').find({ userName: {$in : keywords} });
+            const results = await cursor.toArray();
+            return results;
+        }
     }
 }
 
