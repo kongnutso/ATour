bdae5b9d1ab73f34b81d61a458c29bda40ac50a1
jitimonsun <sun.sjm30@gmail.com>
2018-11-21 00:06:58 +0700

    add guidename and tourname to trips

--- atour-backend/src/domain/Tour.ts
+++ atour-backend/src/domain/Tour.ts
@@ -14,7 +14,7 @@ export type PublishTour = (
 
 export type EditTour = (t: Tour, updateInfo: PartialTour) => Tour;
 
-export type AddTrip = (t: Tour, d: Date) => Tour;
+export type AddTrip = (t: Tour, d: Date, guideName: string) => Tour;
 
 export function publishTour(idGenerator: IdGenerator): PublishTour {
   return (
@@ -50,8 +50,8 @@ export function editTour(): EditTour {
 }
 
 export function addTrip(idGenerator: IdGenerator): AddTrip {
-  return (tour: Tour, tripDate: Date): Tour => {
-    const addedTrip: UnbookedTrip = {_type: TripType.UnbookedTrip, tripId: idGenerator(), tripDate, tourId: tour.tourId };
+  return (tour: Tour, tripDate: Date, guideName:string): Tour => {
+    const addedTrip: UnbookedTrip = {_type: TripType.UnbookedTrip, tripId: idGenerator(), tripDate, tourId: tour.tourId, tourName: tour.tourName,  guideName};
     const newTrips = [...tour.trips, addedTrip];
     return {
       ...tour,
