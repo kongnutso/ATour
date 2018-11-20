3e010fc84594db1b2235e39921ce246f17b17de1
jitimonsun <sun.sjm30@gmail.com>
2018-11-20 15:00:39 +0700

    add tourid to domain

--- atour-backend/src/domain/Trip.ts
+++ atour-backend/src/domain/Trip.ts
@@ -5,7 +5,7 @@ type BookTrip = (
   customerId: string,
   bookDate: Date,
   size: number,
-  price: number
+  price: number,
 ) => BookedTrip;
 
 export function bookTrip(): BookTrip {
@@ -14,9 +14,9 @@ export function bookTrip(): BookTrip {
     customerId: string,
     bookDate: Date,
     size: number,
-    price: number
+    price: number,
   ): BookedTrip => {
-    const { tripId, tripDate } = trip;
-    return { _type: TripType.BookedTrip, tripId, tripDate, bookInfo: { bookDate, customerId, size, price } };
+    const { tripId, tripDate, tourId } = trip;
+    return { _type: TripType.BookedTrip, tripId, tripDate, bookInfo: { bookDate, customerId, size, price } , tourId};
   };
 }
