c72aaec9050069a47d7c78ae58c5f425919aaa6b
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-11-22 09:09:33 +0700

    make cards in published tour the same pattern as search for

--- atour-frontend/src/component/Cards/Cards.js
+++ atour-frontend/src/component/Cards/Cards.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Flex } from 'rebass';
-import CardItem from './CardItem';
-import { Card } from 'semantic-ui-react';
+import React from "react";
+import { Flex } from "rebass";
+import CardItem from "./CardItem";
+import { Card } from "semantic-ui-react";
 
 // target props: tours
 // tourName, tourImage, tourRating, tourPrice, tourLocation
 const Cards = props => {
-  const { isGuide, items } = props;
+  const { isGuide, items, role } = props;
   return (
     <Flex
       style={{
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        justifyContent: 'center'
+        marginLeft: "auto",
+        marginRight: "auto",
+        justifyContent: "center"
       }}
       width={1}
       flexWrap="wrap"
@@ -21,9 +21,9 @@ const Cards = props => {
         ? items.map(item => (
             <div
               key={isGuide ? item.guideId : item.tourId}
-              style={{ margin: '10px 20px 10px 20px' }}
+              style={{ margin: "10px 20px 10px 20px" }}
             >
-              <CardItem item={item} isGuide={isGuide} />
+              <CardItem item={item} isGuide={isGuide} role={role} />
             </div>
           ))
         : null}
